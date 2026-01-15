25166b08fedd0c5a6f7735fe6a3651073f237363
Kim MinSeo <86477146+minseid@users.noreply.github.com>
2026-01-15 09:21:32 +0900

    security3

--- src/couponData.js
+++ src/couponData.js
@@ -1,4 +1,4 @@
-export const COUPON_NOTICE_TEXT = '1/14일 형광석 : 온천산 , 참나무 : 12번 홈';
+export const COUPON_NOTICE_TEXT = '1/15일 형광석 : 5번홈 , 참나무 : 8번 홈';
 
 // 사용 가능한 모든 리딤 코드 목록을 여기에 관리
 export const COUPON_INFO = {
